c7f612b9c68aea3bc24570ebae3ced47f985fed7
jeff kang <jeffyjkang@gmail.com>
2022-09-16 11:25:42 -0500

    deployment

--- third/migrations/2_contracts_migration.js
+++ third/migrations/2_contracts_migration.js
@@ -1,9 +1,9 @@
 const ERC20Token = artifacts.require("ERC20");
 const SupplyChain = artifacts.require("SupplyChain");
-const BasicMath = artifacts.require("BasicMath")
+// const BasicMath = artifacts.require("BasicMath")
 
 module.exports = function (deployer) {
   deployer.deploy(ERC20Token, "SimpleToken", "STN", 18, 10000);
   deployer.deploy(SupplyChain);
-  deployer.deploy(BasicMath);
+  // deployer.deploy(BasicMath);
 }
--- third/truffle-config.js
+++ third/truffle-config.js
@@ -41,10 +41,10 @@
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
-// require('dotenv').config();
-// const { MNEMONIC, PROJECT_ID } = process.env;
+require('dotenv').config();
+const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   /**
@@ -69,6 +69,18 @@ module.exports = {
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC,`https://ropsten.infura.io/v3/${PROJECT_ID}`)
+      },
+      network_id: 3
+    },
+    live: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC,`https://mainnet.infura.io/v3/${PROJECT_ID}`)
+      },
+      network_id: 1
+    }
     //
     // An additional network, but with some advanced options…
     // advanced: {
